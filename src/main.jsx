682ca35063011fcc4f7e0b2e7a22b7da510737d1
Abhishek Verma <gaurav@talbrosaxles.com>
2024-03-24 22:25:13 +0530

    removed react dev tools

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "semantic-ui-css/semantic.min.css";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === "production") {
+  disableReactDevTools();
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <div className="quiz-bodyWrapper">
